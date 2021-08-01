ffea8dc3df2787d1f419b812bed1321b61ca924d
Wadie ELARRIM <wadie.02@gmail.com>
2021-08-01 18:48:48 +0100

    fix(tests): remove nuxtlogo test

--- test/components/TopMenu.spec.js
+++ test/components/TopMenu.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import NuxtLogo from '@/components/NuxtLogo.vue'
+import TopMenu from '@/components/organisms/TopMenu.vue'
 
-describe('NuxtLogo', () => {
+describe('TopMenu', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(NuxtLogo)
+    const wrapper = mount(TopMenu)
     expect(wrapper.vm).toBeTruthy()
   })
 })
